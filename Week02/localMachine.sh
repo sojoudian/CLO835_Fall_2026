7c28694b88dd6b923e0d0ede74d9773e9811920c
BartyCrouch <sojodyan@gmail.com>
2026-09-18 17:09:48 -0400

    update local

--- Week02/localMachine.sh
+++ Week02/localMachine.sh
@@ -8,6 +8,7 @@
 ########################################################
 # 1) Clone
 ########################################################
+rm -rf simple-webapp-flask           # remove an old copy, if any
 git clone https://github.com/sojoudian/simple-webapp-flask
 cd simple-webapp-flask
 
@@ -33,25 +34,31 @@ docker images | grep simple-webapp-flask
 ########################################################
 # 4) Run and test
 ########################################################
+docker rm -f webapp 2>/dev/null      # remove an old container, if any
 docker run -d --name webapp -p 8080:8080 simple-webapp-flask:v1
 docker ps
-curl http://localhost:8080/         # Welcome CLO835!
+
+# Wait for Flask to bind the port, then test:
+until curl -s http://localhost:8080/ ; do sleep 1 ; done ; echo
+# Welcome CLO835!
+
 docker logs webapp
 
 ########################################################
-# 5) Tag
-########################################################
-# Replace <user> with your Docker Hub account name.
-docker tag simple-webapp-flask:v1 <user>/simple-webapp-flask:v1
-
-########################################################
-# 6) Publish for every CPU
+# 5) Publish for every CPU
 ########################################################
 # Step 3 built one image for YOUR CPU. A Mac with Apple Silicon builds arm64,
 # and the EC2 machine is x86_64. That image fails there with "exec format error".
 #
 # buildx builds both, and --push sends both under one name. Docker then picks
-# the correct one on each machine. It replaces the tag and the push above.
+# the correct one on each machine. --push also tags, so no "docker tag" is
+# needed.
+#
+# This needs a builder that supports two platforms. Docker Desktop has one.
+# On plain Docker Engine, run this once first:
+#   docker buildx create --use
+#
+# Replace <user> with your Docker Hub account name.
 docker login
 docker buildx build --platform linux/amd64,linux/arm64 -t <user>/simple-webapp-flask:v1 --push .
 
@@ -59,14 +66,14 @@ docker buildx build --platform linux/amd64,linux/arm64 -t <user>/simple-webapp-f
 docker buildx imagetools inspect <user>/simple-webapp-flask:v1
 
 ########################################################
-# 7) Prove the point
+# 6) Prove the point
 ########################################################
 # On the EC2 machine, or on ANY computer with Docker:
 #   docker run -d -p 8080:8080 <user>/simple-webapp-flask:v1
 # One command repeats all 6 steps of Part 1.
 
 ########################################################
-# 8) Clean up
+# 7) Clean up
 ########################################################
 docker rm -f webapp
 terraform destroy                    # in the Week02 directory
